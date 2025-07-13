823c61cc53894f64a8b361177030f4cc5d26bc9a
iamcheese-man <Ilyassmekarto9@gmail.com>
2025-07-14 00:19:05 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,8 +1,14 @@
 elements.hello_world = {
-	color: "#ff0000",
-	behavior: behaviors.WALL,
-	category: "land",
-	state: "solid",
-}
+    color: "#ff0000",
+    behavior: behaviors.WALL,
+    category: "land",
+    state: "solid",
+    tick: function(pixel) {
+        // Make it heat up and flash randomly
+        pixel.temp += 1;
 
-
+        if (Math.random() < 0.1) {
+            pixel.color = "#"+Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+        }
+    },
+};
